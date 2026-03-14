f4b33f3120a9a9fd770e027a8e24f613de20f8fa
THARUN <tharuntech9840@gmail.com>
2026-03-14 14:01:44 +0530

    header authentication on connection added

--- backend/WebSocket/src/main/java/com/sample/WebSocket/service/WebSocketConfig.java
+++ backend/WebSocket/src/main/java/com/sample/WebSocket/service/WebSocketConfig.java
@@ -1,7 +1,14 @@
 package com.sample.WebSocket.service;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
@@ -23,8 +30,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // The endpoint where clients connect to the WebSocket server
         registry.addEndpoint("/connect/message")
-//                .addInterceptors(new AuthHandshakeInterceptor()) //interceptor is not able to find the header
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    //authenticate header
+    //this interruption check dosent happen for all the chat message, only during initialization it happens
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    // STOMP headers are "native" headers
+                    String authToken = accessor.getFirstNativeHeader("Authorization");
+
+                    if (!"ThisIsASampleSocketProgram!".equals(authToken)) {
+                        throw new IllegalArgumentException("Unauthenticated!");
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
